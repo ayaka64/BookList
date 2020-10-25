681cb0c97e73cb127ddd1cccfc58537c90ec022f
ayaka <asmnkys@gmail.com>
2020-10-25 10:31:44 +0000

    fix show file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,8 +28,11 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        @books = Book.all
+        format.html { render :index, notice: '本の情報が登録されました' }
         format.json { render :show, status: :created, location: @book }
+        
+        #render :index
       else
         format.html { render :new }
         format.json { render json: @book.errors, status: :unprocessable_entity }
@@ -39,27 +42,27 @@ class BooksController < ApplicationController
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
-  def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #def update
+    #respond_to do |format|
+      #if @book.update(book_params)
+        #format.html { redirect_to @book, notice: '本の情報が更新されました' }
+        #format.json { render :show, status: :ok, location: @book }
+      #else
+        #format.html { render :edit }
+        #format.json { render json: @book.errors, status: :unprocessable_entity }
+      #end
+    #end
+  #end
 
   # DELETE /books/1
   # DELETE /books/1.json
-  def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #def destroy
+    #@book.destroy
+    #respond_to do |format|
+      #format.html { redirect_to books_url, notice: '削除しました' }
+      #format.json { head :no_content }
+    #end
+  #end
 
   private
     # Use callbacks to share common setup or constraints between actions.
